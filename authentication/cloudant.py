2f3c77491aac0f64ce4017e4ae80da2ba3cd53de
Khushi <khushisrivastava53@gmail.com>
2020-01-18 19:50:30 +0530

    Added validation for username and password

--- authentication/cloudant.py
+++ authentication/cloudant.py
@@ -4,13 +4,13 @@ from cloudant.document import Document
 
 def create_user_database(user):
     client = Cloudant('869a3a9a-8356-4ae9-8dbf-06e2f727e1ba-bluemix', '76147209959e786263adc8636eb25e3e61edeb63e68d1b7aa0bd183690f2808f', url='https://869a3a9a-8356-4ae9-8dbf-06e2f727e1ba-bluemix.cloudant.com', connect=True, auto_renew=True)
+    #client = Cloudant('khushi', '1234', url='http://127.0.0.1:5984', connect=True, auto_renew=True)
     session = client.session()
     if user['user_type'] == 'school':
         school_database = client.create_database(user['username'])
         user["_id"] = "root:profile"
         school_profile = school_database.create_document(user)
         schools_database = client['schools']
-        # schools_list_document = schools_database[user['subdistrict']]
         with Document(schools_database, user["subdistrict"]) as document:
             document['schools'].update({
 
--- authentication/serializer.py
+++ authentication/serializer.py
@@ -30,29 +30,27 @@ class LoginSerializer(serializers.Serializer):
         password = data.get('password', None)
 
         if username is None:
-            raise serializers.ValidationError(
-                'A Username is required to log in.'
-            )
+            raise serializers.ValidationError('A Username is required to log in.')
         if password is None:
-            raise serializers.ValidationError(
-                'A Password is required to log in.'
-            )
-        usernameVerified = User.objects.get(username=username)
-        if usernameVerified is none:
-            raise serializers.ValidationError(
-                'Username not found, please consider registration first'
-        )
+            raise serializers.ValidationError('A Password is required to log in.')
+        
+        if User.objects.filter(username=username).exists():
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise serializers.ValidationError('Invalid Password')
+        else:
+            raise serializers.ValidationError('Username not Found. Consider Registring First')
 
-        if not User.check_password(password):
-            raise serializers.ValidationError(
-                'Password Invalid'
-        )
+        # if not User.check_password(password=password):
+        #     raise serializers.ValidationError(
+        #         'Password Invalid'
+        # )
 
-        user = authenticate(username=username, password=password)
-        if user is None:
-            raise serializers.ValidationError(
-                'A user with this username and password was not found.'
-            )
+        # user = authenticate(username=username, password=password)
+        # if user is None:
+        #     raise serializers.ValidationError(
+        #         'A user with this username and password was not found.'
+        #     )
 
         return {
             'error': False,
